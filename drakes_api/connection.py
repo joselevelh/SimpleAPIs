f167e414032496e2aaec26e50f96ad067a773d5d
JoseLevel <Jose.levelh@gmail.com>
2023-11-28 07:34:25 -0800

    can connect to db and list db names

--- drakes_api/connection.py
+++ drakes_api/connection.py
@@ -1,4 +1,16 @@
 from pymongo import MongoClient
 
-MONGO_URI = "mongodb+srv://joselevel:ItOnuTIkP7rDsO5Z@cluster1.rdtlrgq.mongodb.net/?retryWrites=true&w=majority"
-client = MongoClient(MONGO_URI)
+MONGO_URI = "mongodb+srv://user2:x7s1uNrJePt7ZvCq@cluster1.rdtlrgq.mongodb.net/?retryWrites=true&w=majority"
+
+try:
+    client = MongoClient(MONGO_URI)
+    # Check the connection by listing database names
+    print(f"Client: {client}")
+    database_names = client.list_databases()
+    print("Connected to MongoDB. Database names:")
+    for db_name in database_names:
+        print(db_name)
+
+
+except Exception as e:
+    print(f"Error connecting to MongoDB: {e}")
