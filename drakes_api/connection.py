30e0c52447b3fafbe23e7d45c90b739e5ff84351
JoseLevel <Jose.levelh@gmail.com>
2023-11-29 08:55:36 -0800

    sample adding documents

--- drakes_api/connection.py
+++ drakes_api/connection.py
@@ -2,6 +2,21 @@ from pymongo import MongoClient
 
 MONGO_URI = "mongodb+srv://user2:x7s1uNrJePt7ZvCq@cluster1.rdtlrgq.mongodb.net/?retryWrites=true&w=majority"
 
+new_accounts = [
+    {
+        "account_id": "MDB011235813",
+        "account_holder": "Ada Lovelace",
+        "account_type": "checking",
+        "balance": 60218,
+    },
+    {
+        "account_id": "MDB829000001",
+        "account_holder": "Muhammad ibn Musa al-Khwarizmi",
+        "account_type": "savings",
+        "balance": 267914296,
+    },
+]
+
 try:
     client = MongoClient(MONGO_URI)
     # Check the connection by listing database names
@@ -10,6 +25,15 @@ try:
     print("Connected to MongoDB. Database names:")
     for db_name in database_names:
         print(db_name)
+    db = client.bank
+    accounts_collection = db.accounts
+    result = accounts_collection.insert_many(new_accounts)
+
+    document_ids = result.inserted_ids
+    print("# of documents inserted: " + str(len(document_ids)))
+    print(f"_ids of inserted documents: {document_ids}")
+
+    client.close()
 
 
 except Exception as e:
